3006d41746d001f22ca9a557b563a89c72e98d5c
Josh Picarazzi <joshpicarazzi@gmail.com>
2022-11-02 20:59:19 -0500

    Player movement initial set up

---
+++ clichecowboy/scripts/player.gd
@@ -0,0 +1,32 @@
+extends KinematicBody2D
+
+export (int) var speed = 150
+
+var velocity = Vector2()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _physics_process(delta):
+	player_input()
+	velocity = move_and_slide(velocity)
+
+func player_input():
+	velocity = Vector2()
+	
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	
+	velocity = velocity.normalized() * speed
