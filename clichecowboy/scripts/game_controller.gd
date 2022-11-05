bc6b354dbdce8c31b4fc43cc3bb2572d261274ef
Josh Picarazzi <joshpicarazzi@gmail.com>
2022-11-05 11:02:33 -0500

    fixed bullet firing, now works properly

--- clichecowboy/scripts/game_controller.gd
+++ clichecowboy/scripts/game_controller.gd
@@ -4,7 +4,7 @@ onready var playerScn = preload("res://scenes/player.tscn")
 onready var bulletScn = preload("res://scenes/bullet.tscn")
 
 var player
-var bullets
+var bullets = []
 
 onready var screenSize = get_viewport_rect().size
 
@@ -16,9 +16,17 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	if player.shoot:
 		player.shoot = false
-		bullets = bulletScn.instance()
-		bullets.position = player.position
-		add_child(bullets)
+		bullets.append(bulletScn.instance())
+		bullets[-1].position = player.position
+		bullets[-1].init_stats(200, Vector2(1, 0), 1, 2)
+		add_child(bullets[-1])
+	
+	if !bullets.empty():
+		for b in bullets:
+			if !b.alive:
+				b.queue_free()
+				bullets.remove(bullets.find(b))
+				
